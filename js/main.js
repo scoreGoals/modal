dc13c22f9743de37dd2a65ccc452538002f7b659
scoreGoals <42836914+scoreGoals@users.noreply.github.com>
2020-04-18 03:50:10 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let modal = document.querySelector('.m-modal');
+
+// Когда пользователь кликает кнопку, открываем модальное окно.
+document.querySelector(".m-btn").onclick = function() {
+    modal.style.display = "block";
+}
+
+// Когда пользователь кликает 'x', закрываем модальное окно.
+document.querySelectorAll(".m-close")[0].onclick = function() {
+    modal.style.display = "none";
+}
+
+// Когда пользователь щелкает 'Esc' закрываем модальное окно.
+document.onkeyup = function(event) {
+    if (event.keyCode === 27) {
+        modal.style.display = "none";
+    }
+}
+
+// Когда пользователь щелкает в любом месте за пределами модального, закрываем его.
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
