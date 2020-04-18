cfc4740bd52c445c051a4e7fb5ad480bae1d68d4
scoreGoals <42836914+scoreGoals@users.noreply.github.com>
2020-04-18 20:14:08 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 let modal = document.querySelector('.m-modal');
+let content = document.querySelector('.m-modal__content');
+let info = document.querySelector('.m-modal__info');
 
 // Когда пользователь кликает кнопку, открываем модальное окно.
 document.querySelector(".m-btn").onclick = function() {
@@ -10,10 +12,11 @@ document.querySelectorAll(".m-close")[0].onclick = function() {
     modal.style.display = "none";
 }
 
-// Когда пользователь кликает по затемненному участку, закрываем модальное окно.
-document.querySelector(".m-modal__bg").onclick = function() {
+// Когда пользователь кликает 'x', закрываем модальное окно.
+document.querySelectorAll(".m-close")[1].onclick = function() {
     modal.style.display = "none";
 }
+
 
 // Когда пользователь щелкает 'Esc', закрываем модальное окно.
 document.onkeyup = function(event) {
@@ -22,9 +25,26 @@ document.onkeyup = function(event) {
     }
 }
 
+// Когда пользователь кликает по затемненному участку, закрываем модальное окно.
+document.querySelector(".m-modal__bg").onclick = function() {
+    modal.style.display = "none";
+}
+
 // Когда пользователь щелкает в любом месте за пределами модального, закрываем его.
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+// Когда пользователь кликает кнопку m-rule, открываем модальное окно.
+document.querySelector(".m-rule").onclick = function() {
+    info.style.display = "block";
+    content.style.display = "none";
+}
+
+// Когда пользователь кликает кнопку m-back, открываем модальное окно.
+document.querySelector(".m-back").onclick = function() {
+    content.style.display = "block";
+    info.style.display = "none";
 }
